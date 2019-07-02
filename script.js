663415b17c709e4d59685c7f459e970fda6a52dd
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-07-02 22:00:32 +0200

    readme.md

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function attribute() {
-    let score = Math.floor(Math.random() * 19);
-    document.getElementById('point').innerHTML = score;
+    let attset = [];
+    for (i = 0; i < 6; i++){
+    attset.push(Math.floor(Math.random() * 19));
+    document.getElementById('point').innerHTML = attset};
 }
