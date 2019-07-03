57cb086d930371fcb6a355efeae35bf66229f382
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-07-03 22:42:58 +0200

    index.html

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function attribute() {
     const pointEls = document.getElementsByClassName('att_score');
     for (i = 0; i < pointEls.length; i++) {
-        pointEls[i].innerHTML = (Math.floor(Math.random() * 19 - 3) + 3);
-    };
-    if (pointEls.value > 10) {
-        document.getElementsByClassName('modifier').innerHTML = '5'
-    } else {
-        document.getElementsByClassName('modifier').innerHTML= '-5'
+        const randomN = Math.floor(Math.random() * (18 - 3 + 1) + 3);
+        pointEls[i].innerHTML = randomN;
+        document.getElementsByClassName('modifier')[i].innerHTML = Math.floor(randomN / 2 - 5);
     };
 }
+
+const Races = {
+    'dwarf' : {
+        height: 50
+    },
+    'High elf' : {
+        height: 250
+    }
+    
+}
+const Alignment = {
+    'Lawful Good': {
+        description: 'creatures can be counted on to do the right thing as expected by society. Gold dragons, paladins, and most dwarves are lawful good.'
+    }
+}
+
+Races['High elf'].height
+
