c37fe93772572276d8a55399f884413af1b0121f
Fredrik Trusohamn <fredrik.trusohamn@gmail.com>
2019-07-03 12:48:47 +0200

    index.html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function attribute() {
-    const pointEls = document.getElementsByClassName('point');
+    const pointEls = document.getElementsByClassName('att_score');
     for (i = 0; i < pointEls.length; i++) {
-        pointEls[i].innerHTML = Math.floor(Math.random() * 19);
+        pointEls[i].innerHTML = (Math.floor(Math.random() * 19 - 3) + 3);
     };
     if (pointEls.value > 10) {
-        document.getElementsByClassName('modifier').innerHTML=-5
+        document.getElementsByClassName('modifier').innerHTML = '5'
     } else {
-        document.getElementsByClassName('modifier').innerHTML=5
+        document.getElementsByClassName('modifier').innerHTML= '-5'
     };
 }
